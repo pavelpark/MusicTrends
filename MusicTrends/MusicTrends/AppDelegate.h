8ac5bba87a9752038a1846119b4efa902c0e417d
Pavel Parkhomey <pavelpark@aol.com>
2017-05-30 14:32:47 -0700

    Created Xcode Project

---
+++ MusicTrends/MusicTrends/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MusicTrends
+//
+//  Created by Pavel Parkhomey on 5/30/17.
+//  Copyright © 2017 Pavel Parkhomey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MusicTrends/MusicTrends/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MusicTrends
+//
+//  Created by Pavel Parkhomey on 5/30/17.
+//  Copyright © 2017 Pavel Parkhomey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
