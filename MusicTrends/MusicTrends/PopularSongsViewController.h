2ebbd063229a5629eff70d51070012237187d96d
Pavel Parkhomey <pavelpark@aol.com>
2017-05-31 11:36:49 -0700

    Created a Xib file for Each Popular song.

---
+++ MusicTrends/MusicTrends/PopularSongsViewController.h
@@ -0,0 +1,13 @@
+//
+//  PopularSongsViewController.h
+//  MusicTrends
+//
+//  Created by Pavel Parkhomey on 5/31/17.
+//  Copyright © 2017 Pavel Parkhomey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PopularSongsViewController : UIViewController
+
+@end
---
+++ MusicTrends/MusicTrends/PopularSongsViewController.m
@@ -0,0 +1,37 @@
+//
+//  PopularSongsViewController.m
+//  MusicTrends
+//
+//  Created by Pavel Parkhomey on 5/31/17.
+//  Copyright © 2017 Pavel Parkhomey. All rights reserved.
+//
+
+#import "PopularSongsViewController.h"
+
+@interface PopularSongsViewController ()
+
+@end
+
+@implementation PopularSongsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
