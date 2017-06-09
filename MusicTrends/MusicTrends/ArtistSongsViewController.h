1547300e180d14a878a6d0061cd0973562a57ddf
Pavel Parkhomey <pavelpark@aol.com>
2017-06-08 22:14:19 -0700

    Created another ViewController to Display Artists Popular songs.

---
+++ MusicTrends/MusicTrends/ArtistSongsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ArtistSongsViewController.h
+//  MusicTrends
+//
+//  Created by Pavel Parkhomey on 6/8/17.
+//  Copyright © 2017 Pavel Parkhomey. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ArtistSongsViewController : ViewController
+
+@end
